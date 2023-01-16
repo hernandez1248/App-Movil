d44ad581d98b5a334e5001d164773134845d31ca
alexisSalmeron <alexissalmeron.baez9@gmail.com>
2023-01-15 22:43:26 -0600

    Unidades y cronograma

---
+++ components/enlaces.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { MapFill, BusFrontFill, JournalText} from 'react-bootstrap-icons';
+
+
+function Enlaces() {
+  return (
+    <>
+      <Navbar border="dark" bg="light" variant="light" fixed="bottom">
+        <Container>
+          <Nav className="footer">
+            <Nav.Link href="#" ><MapFill className="footer-icons"></MapFill></Nav.Link>
+            <Nav.Link href="/unidades" ><BusFrontFill className="footer-icons"></BusFrontFill></Nav.Link>
+            <Nav.Link href="/cronograma-admin"><JournalText className="footer-icons"></JournalText></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Enlaces;
