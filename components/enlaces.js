7a49afbfbb14e365a2488dc9fbbad68527f4d95d
Luis Alberto <92551372+Betomx45@users.noreply.github.com>
2023-01-17 22:19:07 -0600

    interfaces usuario

--- components/enlaces.js
+++ components/enlaces.js
@@ -10,9 +10,11 @@ function Enlaces() {
       <Navbar border="dark" bg="light" variant="light" fixed="bottom">
         <Container>
           <Nav className="footer">
-            <Nav.Link href="/rutas" ><MapFill className="footer-icons"></MapFill></Nav.Link>
+            <Nav.Link href="/rutas-admin" ></Nav.Link>
+            <Nav.Link href="/rutas-usuario" ><MapFill className="footer-icons"></MapFill></Nav.Link>
             <Nav.Link href="/unidades" ><BusFrontFill className="footer-icons"></BusFrontFill></Nav.Link>
             <Nav.Link href="/cronograma-admin"><JournalText className="footer-icons"></JournalText></Nav.Link>
+            <Nav.Link href="/cronograma-user"></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
