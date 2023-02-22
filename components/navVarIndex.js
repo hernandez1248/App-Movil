97f430d4804bd360068c94a6427e19b67b0fd6d7
AlexisSalmeron <alexissalmeron.baez9@gmail.com>
2023-02-22 16:32:39 -0600

    FrontEnd RegistroUsuarios

---
+++ components/navVarIndex.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function NavVarIndex() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Usuarios SIRTA
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
